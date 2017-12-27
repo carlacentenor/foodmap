1908dddb3e6b1df17150fd3c078bca4f7ad7b0d4
carlacentenor <carlacentenor@gmail.com>
2017-12-26 21:59:42 -0500

    agregando filtro por nombre

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
- 
-  $('#splash-js').delay(2500).fadeOut('slow');
+// efecto splash
+$('#splash-js').delay(2500).fadeOut('slow');
 
+// Obteniendo información
+var container = $('.row');
+// creando los contenedores de información por cada restaurante
+for (i = 0; i < data.length; i++) {
+  container.append('<div class="col-xs-5 box-restaurant collection"><p class="name-restaurant">' + data[i].name + '</p><img class="img-restaurant"  src=' + data[i].image + '><div class="opacity"></div> </div>');
+}
+// Filtrando por Nombre 
+$('.search').keyup(function() {
+  var name = $(this).val();
+  $('.collection').hide();
+  $('.collection').each(function() {
+    var search = $(this).text();
+    if (search.indexOf(name) !== -1) {
+      $(this).show();
+    }
+  });
+});
---
+++ js/data.js
@@ -0,0 +1,116 @@
+var data = [{
+
+
+  'name': 'la vaca loca',
+  'type': 'meat',
+  'stars': 3,
+  'address': 'Miraflores',
+  'description': 'Av. Larco 5634',
+  'money': '$',
+  'image': 'assets/images/carne.jpg'
+
+},
+{
+  'name': 'mangos',
+  'type': 'fusion',
+  'stars': 5,
+  'address': 'Miraflores',
+  'description': 'Larcomar 1234',
+  'money': '$$$',
+  'image': 'assets/images/fusion1.jpg'
+
+},
+{
+  'name': 'punta sal',
+  'type': 'fish',
+  'stars': 4,
+  'address': 'Miraflores',
+  'description': 'La Paz 593',
+  'money': '$$$',
+  'image': 'assets/images/fish.jpg'
+
+},
+{
+  'name': 'punto azul',
+  'type': 'fish',
+  'stars': 3,
+  'address': 'Miraflores',
+  'description': 'San Martin 305',
+  'money': '$$$',
+  'image': 'assets/images/fish2.jpg'
+
+},
+{
+  'name': 'sushi pop',
+  'type': 'japanese',
+  'stars': 3,
+  'address': 'Miraflores',
+  'description': 'Av. Porta 408',
+  'money': '$$',
+  'image': 'assets/images/japon.jpg'
+},
+{
+  'name': 'barra mar',
+  'type': 'fish',
+  'stars': 5,
+  'address': 'Barranco',
+  'description': 'Grau  1234',
+  'money': '$$$',
+  'image': 'assets/images/fish.jpg'
+},
+{
+  'name': 'cala',
+  'type': 'fusion',
+  'stars': 4,
+  'address': 'Barranco',
+  'description': 'Malecon Balta 784',
+  'money': '$$',
+  'image': 'assets/images/fusion1.jpg'
+},
+{
+  'name': 'la cuadra de salvador',
+  'type': 'meat',
+  'stars': 4,
+  'address': 'Barranco',
+  'description': 'La Encantada 233',
+  'money': '$$$',
+  'image': 'assets/images/carne.jpg'
+},
+{
+  'name': 'shen',
+  'type': 'japanese',
+  'stars': 3,
+  'address': 'Barranco',
+  'description': 'Miguel Iglesias 987',
+  'money': '$$$',
+  'image': 'assets/images/japon2.jpg'
+},
+{
+  'name': 'barra lima',
+  'type': 'fish',
+  'stars': 5,
+  'address': 'San Isidro',
+  'description': 'Av. General Pezet 734',
+  'money': '$$$',
+  'image': 'assets/images/fish2.jpg'
+},
+{
+  'name': 'dondoh robata grill',
+  'type': 'meat',
+  'stars': 3,
+  'address': 'San Isidro',
+  'description': 'Av. Salaverry 1934',
+  'money': '$$',
+  'image': 'assets/images/carne.jpg'
+},
+{
+  'name': 'nanka',
+  'type': 'fusion',
+  'stars': 4,
+  'address': 'San Isidro',
+  'description': 'Av. Javier Prado 1196',
+  'money': '$$',
+  'image': 'assets/images/fusion2.jpg'
+}
+
+];
