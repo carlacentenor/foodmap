a1c70db98fefe057431e71e930be2d6ca16e25e4
carlacentenor <carlacentenor@gmail.com>
2017-12-27 12:40:41 -0500

    filtrado por tipo de comida terminado

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@ $('#splash-js').delay(2500).fadeOut('slow');
 
 // Obteniendo información
 var container = $('.row');
-// creando los contenedores de información por cada restaurante
-for (i = 0; i < data.length; i++) {
-  container.append('<div class="col-xs-5 box-restaurant collection"><p class="name-restaurant">' + data[i].name + '</p><img class="img-restaurant"  src=' + data[i].image + '><div class="opacity"></div> </div>');
+// Función que muestra todos los restaurantes
+function allFood() {
+  for (i = 0; i < data.length; i++) {
+    container.append('<div class="col-xs-5 col-md-2 box-restaurant collection"><p class="name-restaurant">' + data[i].name + '</p><img class="img-restaurant"  src=' + data[i].image + '><div class="opacity"></div> </div>');
+  }
 }
+// Ejecutando la función al iniciar
+allFood();
+
 // Filtrando por Nombre 
 $('.search').keyup(function() {
   var name = $(this).val();
@@ -18,3 +23,42 @@ $('.search').keyup(function() {
     }
   });
 });
+
+/** Filtrando por tipo */
+
+$('#food').change(function() {
+  var selectedOption = $('#food option:selected');
+  $('.collection').remove();
+  if (selectedOption.val() === 'fish') {
+    create('fish');
+  }
+
+  if (selectedOption.val() === 'meat') {
+    create('meat');
+  }
+
+  if (selectedOption.val() === 'japanese') {
+    create('japanese');
+  }
+
+  if (selectedOption.val() === 'fusion') {
+    create('fusion');
+  }
+
+  if (selectedOption.val() === 'all') {
+    allFood();
+  }
+});
+
+/* *Función que filtra por tipo de comida */
+function create(food) {
+  var type = [];
+  for (j = 0; j < data.length; j++) {
+    if (data[j].type === food) {
+      type.push(data[j]);
+    }
+  }
+  for (k = 0; k < type.length; k++) {
+    container.append('<div class="col-xs-5 col-md-2 box-restaurant collection"><p class="name-restaurant">' + type[k].name + '</p><img class="img-restaurant"  src=' + type[k].image + '><div class="opacity"></div> </div>');
+  }
+}
