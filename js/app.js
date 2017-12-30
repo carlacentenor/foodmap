13c1774cd0cd64c9934f776c69a4392255d3bc32
carlacentenor <carlacentenor@gmail.com>
2017-12-29 19:08:21 -0500

    mouseiver texto listo

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var modal = $('.modal');
 // Función que muestra todos los restaurantes
 function allFood() {
   for (i = 0; i < data.length; i++) {
-    container.append('<div class="col-xs-5 col-md-2 box-restaurant collection" data-name="' + data[i].name + '" data-type="' + data[i].type + '" data-city="' + data[i].address + '" data-toggle="modal" data-target="#myModal" ><p class="name-restaurant">' + data[i].name + '</p><img class="img-restaurant"  src=' + data[i].image + '><div class="opacity"></div> </div>');
+    container.append('<div class="col-xs-5 col-md-2 box-restaurant collection" data-name="' + data[i].name + '" data-type="' + data[i].type + '" data-city="' + data[i].address + '" data-toggle="modal" data-target="#myModal" ><p class="name-restaurant">' + data[i].name + '</p><p class="popular"></p><img class="img-restaurant"  src=' + data[i].image + '><div class="opacity"></div> </div>');
   }
 }
 // Ejecutando la función al iniciar
@@ -42,14 +42,29 @@ $('.search').keyup(function() {
 
 /** Contenido Modal*/
 $('.box-restaurant').on('click', function() {
+  $('.icon-star').remove();
   for (i = 0; i < data.length;i++) {
     if ($(this).data('name') === data[i].name) {
       $('.logo-restaurant').attr('src', data[i].logo);
       $('.local-restaurant').attr('src', data[i].local);
       $('.address-restaurant').text(data[i].description);
       $('.city-restaurant').text(data[i].address);
-      $('.star-restaurant').text(data[i].stars);
+      
       $('.price-restaurant').text(data[i].money);
+      for (j = 0; j < data[i].stars;j++) {
+        $('.star-restaurant').append('<span class="glyphicon glyphicon-star icon-star"></span>');
+      }
     }
   }
 });
+
+$('.box-restaurant').on('mouseover', function() {
+  $('.white').remove();
+  for (i = 0; i < data.length;i++) {
+    if ($(this).data('name') === data[i].name) {
+      $(this).append('<p class="white">' + data[i].address + '</p>');
+    }
+  }
+});
+
+
